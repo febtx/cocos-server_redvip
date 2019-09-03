6213312ce1f36d52fdfc310194d68ca0294f0b9e
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-09-03 12:33:59 +0700

    update  03/09/2019

--- app/Controllers/admin/panel/HeThong/clear.js
+++ app/Controllers/admin/panel/HeThong/clear.js
@@ -1,6 +1,6 @@
 
 // OTP
-var OTP                = require('../../../../Models/OTP');
+var OTP              = require('../../../../Models/OTP');
 
 /**
 // Mua Xu
@@ -18,43 +18,51 @@ var ChuyenRed          = require('../../../../Models/ChuyenRed');
 */
 
 // Gift Code
-var GiftCode           = require('../../../../Models/GiftCode');
+var GiftCode         = require('../../../../Models/GiftCode');
 
 // Tài Xỉu
-var TaiXiu_phien       = require('../../../../Models/TaiXiu_phien');
-var TaiXiu_one         = require('../../../../Models/TaiXiu_one');
-var TaiXiu_cuoc        = require('../../../../Models/TaiXiu_cuoc');
-var TaiXiu_chat        = require('../../../../Models/TaiXiu_chat');
+var TaiXiu_phien     = require('../../../../Models/TaiXiu_phien');
+var TaiXiu_one       = require('../../../../Models/TaiXiu_one');
+var TaiXiu_cuoc      = require('../../../../Models/TaiXiu_cuoc');
+var TaiXiu_chat      = require('../../../../Models/TaiXiu_chat');
 
 // AngryBirds
-var AngryBirds_red     = require('../../../../Models/AngryBirds/AngryBirds_red');
-var AngryBirds_xu      = require('../../../../Models/AngryBirds/AngryBirds_xu');
+var AngryBirds_red   = require('../../../../Models/AngryBirds/AngryBirds_red');
+var AngryBirds_xu    = require('../../../../Models/AngryBirds/AngryBirds_xu');
 
 // BauCua
-var BauCua_phien       = require('../../../../Models/BauCua/BauCua_phien');
-var BauCua_cuoc        = require('../../../../Models/BauCua/BauCua_cuoc');
+var BauCua_phien     = require('../../../../Models/BauCua/BauCua_phien');
+var BauCua_cuoc      = require('../../../../Models/BauCua/BauCua_cuoc');
 
 // BigBabol
-var BigBabol_red       = require('../../../../Models/BigBabol/BigBabol_red');
-var BigBabol_xu        = require('../../../../Models/BigBabol/BigBabol_xu');
+var BigBabol_red     = require('../../../../Models/BigBabol/BigBabol_red');
+var BigBabol_xu      = require('../../../../Models/BigBabol/BigBabol_xu');
 
 // CaoThap
-var CaoThap_red        = require('../../../../Models/CaoThap/CaoThap_red');
-var CaoThap_xu         = require('../../../../Models/CaoThap/CaoThap_xu');
-var CaoThap_redbuoc    = require('../../../../Models/CaoThap/CaoThap_redbuoc');
-var CaoThap_xubuoc     = require('../../../../Models/CaoThap/CaoThap_xubuoc');
+var CaoThap_red      = require('../../../../Models/CaoThap/CaoThap_red');
+var CaoThap_xu       = require('../../../../Models/CaoThap/CaoThap_xu');
+var CaoThap_redbuoc  = require('../../../../Models/CaoThap/CaoThap_redbuoc');
+var CaoThap_xubuoc   = require('../../../../Models/CaoThap/CaoThap_xubuoc');
 
 // Mini3Cay
-var Mini3Cay_red       = require('../../../../Models/Mini3Cay/Mini3Cay_red');
-var Mini3Cay_xu        = require('../../../../Models/Mini3Cay/Mini3Cay_xu');
+var Mini3Cay_red     = require('../../../../Models/Mini3Cay/Mini3Cay_red');
+var Mini3Cay_xu      = require('../../../../Models/Mini3Cay/Mini3Cay_xu');
 
 // miniPoker
-var miniPokerRed       = require('../../../../Models/miniPoker/miniPokerRed');
-var miniPokerXu        = require('../../../../Models/miniPoker/miniPokerXu');
+var miniPokerRed     = require('../../../../Models/miniPoker/miniPokerRed');
+var miniPokerXu      = require('../../../../Models/miniPoker/miniPokerXu');
 
 // VuongQuocRed
-var VuongQuocRed_red   = require('../../../../Models/VuongQuocRed/VuongQuocRed_red');
-var VuongQuocRed_xu    = require('../../../../Models/VuongQuocRed/VuongQuocRed_xu');
+var VuongQuocRed_red = require('../../../../Models/VuongQuocRed/VuongQuocRed_red');
+var VuongQuocRed_xu  = require('../../../../Models/VuongQuocRed/VuongQuocRed_xu');
+
+// Candy
+var Candy_red        = require('../../../../Models/Candy/Candy_red');
+var Candy_xu         = require('../../../../Models/Candy/Candy_xu');
+
+// LongLan
+var LongLan_red      = require('../../../../Models/LongLan/LongLan_red');
+var LongLan_xu       = require('../../../../Models/LongLan/LongLan_xu');
 
 module.exports = function() {
 	// OTP
@@ -123,4 +131,12 @@ module.exports = function() {
 	// VuongQuocRed
 	VuongQuocRed_red.deleteMany({'time':{$lt: timeDay}}).exec();
 	VuongQuocRed_xu.deleteMany({'time': {$lt: timeDay}}).exec();
+
+	// Candy
+	Candy_red.deleteMany({'time':{$lt: timeDay}}).exec();
+	Candy_xu.deleteMany({'time': {$lt: timeDay}}).exec();
+
+	// LongLan
+	LongLan_red.deleteMany({'time':{$lt: timeDay}}).exec();
+	LongLan_xu.deleteMany({'time': {$lt: timeDay}}).exec();
 }
