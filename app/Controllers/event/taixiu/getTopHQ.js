7fb3ed0a4abc6a9914e79ae2379b0882c03b4aa5
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-09-22 21:49:01 +0700

    update 22/09/2019

--- app/Controllers/event/taixiu/getTopHQ.js
+++ app/Controllers/event/taixiu/getTopHQ.js
@@ -5,8 +5,8 @@ let TaiXiu_event = require('../../../Models/TaiXiu/TaiXiu_event');
 let UserInfo     = require('../../../Models/UserInfo');
 
 module.exports = function(client, date){
-	var topWin  = TaiXiu_event.find({date: date, win: true}, {}, {sort:{'line':-1, 'last':-1}}).exec();
-	var topLost = TaiXiu_event.find({date: date, win: false}, {}, {sort:{'line':-1, 'last':-1}}).exec();
+	var topWin  = TaiXiu_event.find({date: date, win: true}, {}, {sort:{'top':-1}}).exec();
+	var topLost = TaiXiu_event.find({date: date, win: false}, {}, {sort:{'top':-1}}).exec();
 
 	Promise.all([topWin, topLost])
 	.then(result => {
--- app/Models/OTP.js
+++ app/Models/OTP.js
@@ -2,11 +2,13 @@
 let mongoose = require("mongoose");
 
 let Schema = new mongoose.Schema({
-	uid:    {type: String,  required: true, index: true}, // ID người dùng
+	uid:    {type: String,  required: true}, // ID người dùng
 	phone:  {type: String,  required: true}, // Số điện thoại
 	code:   {type: String,  required: true}, // Mã OTP
 	active: {type: Boolean, default: false}, // Trạng thái sử dụng
     date:   {type: Date,    required: true}, // Thời gian tạo
 });
+
+Schema.index({uid: 1, phone: 1}, {background: true});
 
 module.exports = mongoose.model("OTP", Schema);
