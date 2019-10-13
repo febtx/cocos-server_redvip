b3c1c78fd1fa564988c2cdb434fc6ca3b2b29bc0
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-10-13 12:57:09 +0700

    update 13/10/2019

--- app/Controllers/game/XocXoc.js
+++ app/Controllers/game/XocXoc.js
@@ -2,6 +2,7 @@
 let ingame  = require('./XocXoc/ingame');
 let outgame = require('./XocXoc/outgame');
 let cuoc    = require('./XocXoc/cuoc');
+let history = require('./XocXoc/history');
 
 module.exports = function(client, data){
 	if (!!data.ingame) {
@@ -12,6 +13,9 @@ module.exports = function(client, data){
 	}
 	if (!!data.cuoc) {
 		cuoc(client, data.cuoc);
+	}
+	if (!!data.log) {
+		history(client, data.log);
 	}
 	client = null;
 	data   = null;
