8cc1762f24864e3a8a9743fd62a01d284d4bbd1e
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-10-28 17:15:26 +0700

    update 28/10/2019

--- app/Controllers/game/XoSo/mb.js
+++ app/Controllers/game/XoSo/mb.js
@@ -1,7 +1,12 @@
 
-let lo2  = require('./mb/lo2so');
+let lo2     = require('./mb/lo2so');
+let history = require('./mb/history');
 
 module.exports = function(client, data){
+	console.log(data);
+	if (!!data.history) {
+		history(client, data.history);
+	}
 	if (!!data.lo2) {
 		lo2(client, data.lo2);
 	}
