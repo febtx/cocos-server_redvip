63fda4fb279c8bf1e94812bc1bdc31313bea6503
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-10-31 23:05:31 +0700

    update 31/10/2019

--- app/Controllers/shop/get_daily.js
+++ app/Controllers/shop/get_daily.js
@@ -2,6 +2,6 @@
 var tabDaiLy   = require('../../Models/DaiLy');
 module.exports = function(client){
 	tabDaiLy.find({}, function(err, daily){
-		client.red({shop:{chuyen_red:{daily:daily}}});
+		client.red({shop:{daily:daily}});
 	});
 }
