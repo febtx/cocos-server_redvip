20ab8414e3d671daa9604d4eb3846b24c27bab5a
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-01-20 15:26:14 +0700

    update 20/01/2020

--- app/Controllers/admin/game/angrybirds.js
+++ app/Controllers/admin/game/angrybirds.js
@@ -1,10 +1,12 @@
 
-var get_data = require('./angrybirds/get_data');
-var get_top  = require('./angrybirds/get_top');
-var name_hu  = require('./angrybirds/name_hu');
-var getEvent = require('./angrybirds/getEvent');
-var setEvent = require('./angrybirds/setEvent');
-var setChedo = require('./angrybirds/setChedo');
+let get_data = require('./angrybirds/get_data');
+let get_top  = require('./angrybirds/get_top');
+let name_hu  = require('./angrybirds/name_hu');
+let getEvent = require('./angrybirds/getEvent');
+let setEvent = require('./angrybirds/setEvent');
+let setChedo = require('./angrybirds/setChedo');
+let reset_top  = require('./angrybirds/reset_top');
+let reset_data = require('./angrybirds/reset_data');
 
 module.exports = function(client, data) {
 	if (!!data.get_data) {
@@ -24,5 +26,11 @@ module.exports = function(client, data) {
 	}
 	if (void 0 !== data.chedo) {
 		setChedo(client, data.chedo);
+	}
+	if (void 0 !== data.reset_top) {
+		reset_top(client);
+	}
+	if (void 0 !== data.reset_data) {
+		reset_data(client);
 	}
 }
